138315c4ae2aed13899acea89b30822937813cde
Chabam <fchabot1337@gmail.com>
2019-01-19 12:29:32 -0500

    On utilise maintenant 'create-react-app' pour les dépendances

---
+++ src/components/Vecpad.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Vecpad extends Component {
+  render() {
+    return (
+      <div className="vecpad-container">
+      </div>
+    );
+  }
+}
+
+export default Vecpad;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Vecpad from './components/Vecpad';
+
+ReactDOM.render(<Vecpad />, document.getElementById('root'));
