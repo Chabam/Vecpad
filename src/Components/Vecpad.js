ecdd499b30fa91f508768706a0fb73dbaca657c3
Chabam <fchabot1337@gmail.com>
2019-02-04 17:20:04 -0500

    Finalement on a même pas besoin de state! (NOUS SOMMES STATELESS!)

--- src/Components/Vecpad.js
+++ src/Components/Vecpad.js
@@ -7,27 +7,24 @@ import THREEHelper from '../THREE/THREEHelper';
 export default class Vecpad extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {
-			THREEHelper: new THREEHelper()
-		};
+		this.THREEHelper = new THREEHelper()
 	}
 
 	render() {
-		const { THREEHelper } = this.state;
-		let addBasicTriangle = () => THREEHelper.addTriangle(1, 0xffffff, 0x000000, 'Basic Triangle');
-		let addBasicQuad = () => THREEHelper.addQuad(1, 1, 0xffffff, 0x000000, 'Basic Quad');
-		let addBasicCube = () => THREEHelper.addCube(1, 1, 1, 0xffffff, 0x000000, 'Basic Cube');
+		let addBasicTriangle = () => this.THREEHelper.addTriangle(1, 0xffffff, 0x000000, 'Basic Triangle');
+		let addBasicQuad = () => this.THREEHelper.addQuad(1, 1, 0xffffff, 0x000000, 'Basic Quad');
+		let addBasicCube = () => this.THREEHelper.addCube(1, 1, 1, 0xffffff, 0x000000, 'Basic Cube');
 		return (
 			<div id="vecpad-container">
 				<div id="visualizer-container">
 					<Toolbar
-						displayMode={THREEHelper.currentDisplayMode}
-						updateDisplayMode={THREEHelper.setDisplayMode}
+						displayMode={this.THREEHelper.currentDisplayMode}
+						updateDisplayMode={this.THREEHelper.setDisplayMode}
 						addBasicTriangle={addBasicTriangle}
 						addBasicQuad={addBasicQuad}
-						addBasicCube={addBasicCube}
-						></Toolbar>
-					<Visualizer initializeTHREE={THREEHelper.init}></Visualizer>
+						addBasicCube={addBasicCube}>
+					</Toolbar>
+					<Visualizer initializeTHREE={this.THREEHelper.init}></Visualizer>
 				</div>
 			<Sidebar></Sidebar>
 			</div>
